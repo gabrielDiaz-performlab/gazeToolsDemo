065e9f210d4788760fd65addc0edd3477f755e4d
Gabriel J. Diaz <gabriel.diaz@rit.edu>
2015-09-15 14:51:24 -0400

    Fixed an issue hiding a gaze vector

---
+++ gazeTools.py
@@ -0,0 +1,247 @@
+﻿import viz
+import vizact
+import vizshape
+import smi_beta
+import vizconnect
+import numpy as np
+import math
+
+class gazeVector():
+	def __init__(self, eyeTracker, eye, parentNode, renderToWindows = None,gazeVectorColor=viz.RED):
+		
+		self.eye = eye
+		self.eyeTracker = eyeTracker
+		self.renderToWindows = renderToWindows
+		
+		#Creating a line
+		viz.startLayer(viz.LINES)
+		viz.lineWidth(4)#Set the size of the lines. 
+		viz.vertex(0,0,0)
+		viz.vertex(0,0,3)
+		viz.vertexColor(gazeVectorColor)
+		self.gazeLine = viz.endLayer()
+		self.gazeLine.visible(True)
+		#self.gazeLine.setScale(1,1,1)
+
+		if( self.renderToWindows ):
+			self.gazeLine.renderOnlyToWindows(self.renderToWindows)
+		self.gazeLine.setParent(parentNode)
+		
+
+	def toggleUpdate(self):
+		
+		def moveGazeVector():
+			
+			gazeSamp = []
+			
+			gazeSamp = self.eyeTracker.getLastSample()
+			
+			if( gazeSamp is None ):
+				return
+				
+			if( self.eye == viz.LEFT_EYE):
+				gazeSamp = gazeSamp.leftEye;
+			elif( self.eye == viz.RIGHT_EYE ):
+				gazeSamp = gazeSamp.rightEye;
+			
+			
+			#3D gaze is provided as a normalized gaze direction vector (gazeDirection) and a gaze base point (gazeBasePoint).
+			#Gaze base point is given in mm with respect to the origin of the eyetracker coordinate system.
+			# Note: you must flip X
+			viewPos_XYZ = np.array(viz.MainView.getPosition(), dtype = float)
+			gazeDir_XYZ = np.array([ -gazeSamp.gazeDir_x, gazeSamp.gazeDir_y, gazeSamp.gazeDir_z], dtype = float)
+			pupilPos_XYZ = [-gazeSamp.pupilPos_x, gazeSamp.pupilPos_y, gazeSamp.pupilPos_z]
+			pupilPos_XYZ = np.divide(pupilPos_XYZ, 1000)
+
+			# Create a node3D
+			#gazePoint_XYZ = [viewPos_XYZ[0] + gazeDir_XYZ[0], viewPos_XYZ[1] + gazeDir_XYZ[1], viewPos_XYZ[2] + gazeDir_XYZ[2]]
+			gazePoint_XYZ = [gazeDir_XYZ[0], gazeDir_XYZ[1], gazeDir_XYZ[2]]
+			#gazePoint_XYZ = np.multiply(1.0, gazePoint_XYZ)
+			
+			#self.gazeLine.setVertex(0, pupilPos_XYZ[0], pupilPos_XYZ[1], pupilPos_XYZ[2], viz.ABS_PARENT)
+			self.gazeLine.setVertex(0, 0, 0, viz.ABS_PARENT)
+			self.gazeLine.setVertex(1, gazePoint_XYZ[0], gazePoint_XYZ[1], gazePoint_XYZ[2], viz.ABS_PARENT)
+			
+			#print 'GazePoint=[', gazePoint_XYZ, '],[', pupilPos_XYZ,']' 
+			
+			
+#		self.node3D.enable(viz.RENDERING)
+		
+		self.updateAct = vizact.onupdate(viz.PRIORITY_INPUT+1,moveGazeVector)
+
+class gazeSphere():
+	def __init__(self,eyeTracker,eye,parentNode,renderToWindows = None,sphereColor=viz.RED):
+		
+		self.sizeInDegrees = 0.5
+		self.sphereDistance = 1
+		self.renderToWindows = renderToWindows
+		from math import tan, radians
+		
+		self.radius = tan(radians(self.sizeInDegrees)) * self.sphereDistance
+		#with viz.cluster.MaskedContext(viz.CLIENT1):
+		self.node3D = vizshape.addSphere(radius=self.radius, color = sphereColor, alpha = 0.7)
+		
+		if( self.renderToWindows ):
+			self.node3D.renderOnlyToWindows(self.renderToWindows)
+		
+		self.node3D.disable(viz.RENDERING)
+		
+		self.updateAct = []
+		
+		self.eyeTracker = eyeTracker
+		self.eye = eye
+		self.node3D.setParent(parentNode)
+		
+		
+	def toggleUpdate(self):
+	
+		def moveGazeSphere():
+			
+			gazeSamp = []
+			
+			#if( self.eye == viz.BOTH_EYE):
+			gazeSamp = self.eyeTracker.getLastSample()
+			
+			if( gazeSamp is None ):
+				return
+				
+			#timestamp = gazeSamp.timestamp
+			
+			if( self.eye == viz.LEFT_EYE):
+				gazeSamp = gazeSamp.leftEye;
+			elif( self.eye == viz.RIGHT_EYE ):
+				gazeSamp = gazeSamp.rightEye;
+			
+			
+			#3D gaze is provided as a normalized gaze direction vector (gazeDirection) and a gaze base point (gazeBasePoint).
+			#Gaze base point is given in mm with respect to the origin of the eyetracker coordinate system.
+			# Note: you must flip X
+			gazeDirXYZ = [ -gazeSamp.gazeDir_x, gazeSamp.gazeDir_y, gazeSamp.gazeDir_z]
+			gazePointXYZ = self.sphereDistance * gazeDirXYZ
+			
+			#with viz.cluster.MaskedContext(viz.CLIENT1):# show
+			self.node3D.setPosition( gazePointXYZ,viz.ABS_PARENT)
+			
+			
+		self.node3D.enable(viz.RENDERING)
+		
+		self.updateAct = vizact.onupdate(viz.PRIORITY_INPUT+1,moveGazeSphere)
+
+
+class calibrationTools():
+	
+	def __init__(self, parentNode):
+
+		self.calibrationInProgress = False
+		self.parentNode = parentNode
+		self.minimumAngle = (15.0 * np.pi)/(60*180) # We want the calibration point subtend 5 arcmin to the subject's eye
+		self.calibrationSphereRadius = 0.02
+		#self.calibrationSphere = vizshape.addSphere(self.calibrationSphereRadius, color = viz.PURPLE)
+		#self.setSphereRadius(0, 0, 0.02)
+	def dotproduct(self, v1, v2):
+	  return sum((a*b) for a, b in zip(v1, v2))
+	def length(self, v):
+	  return math.sqrt(self.dotproduct(v, v))
+
+	def angle(self, v1, v2):
+	  return math.acos(self.dotproduct(v1, v2) / (self.length(v1) * self.length(v2)))
+
+	def calculateAngularError(self, node1, node2, textObject):
+		#V1 = Ball_Pos_XYZ[:,FrameNumber] - cyclopEyeNode.getPosition(viz.ABS_GLOBAL)
+		#V2 = eyeGazeSphere.getPosition(viz.ABS_GLOBAL) - cyclopEyeNode.getPosition(viz.ABS_GLOBAL)
+		#V2 = [a - b for a, b in zip(eyeGazeSphere.getPosition(viz.ABS_GLOBAL), cyclopEyeNode.getPosition(viz.ABS_GLOBAL))]
+		# XYZ = [l1[idx][0] - l2[idx][0] , l1[idx][1] - l2[idx][1], l1[idx][2] - l2[idx][2]  for idx in range(len(l1))]
+		vector1 = node1.getPosition(viz.ABS_PARENT)
+		#V1 = [a - b for a, b in zip(vector1, self.parentNode.getPosition(viz.ABS_GLOBAL))]
+		V1 = vector1
+		if (node2 == 0.0):
+			V2 = self.calibrationPositions[self.calibrationCounter,:]# + self.parentNode.getPosition(viz.ABS_GLOBAL)
+		else:
+			#V2 = vector2.getPosition()
+			V2 = [b - a for a, b in zip(node1.getPosition(viz.ABS_GLOBAL), node2.getPosition(viz.ABS_GLOBAL))]
+		self.errorAngle = np.multiply(self.angle(V1,V2), 180/np.pi)
+		#print 'Angular Error = %.2f %c'%(errorAngle, u"\u00b0")
+		#print 'Angular Error = %.2f %c'%(self.errorAngle, u"\u00b0")
+		textObject.message('AE = %.2f %c'%(self.errorAngle, u"\u00b0"))
+		textObject.setPosition(-3, 0, 5, viz.ABS_PARENT)
+		if ( self.errorAngle < 15 ):
+			textObject.color(self.errorAngle/15.0, 1 - self.errorAngle/15.0,0)
+		else:
+			textObject.color(1, 0, 0)
+
+
+	def setVolume(self, xRange, yRange, zRange, resolution):
+		'''
+		Output:  positions of the grid points
+		
+		Here, we create a calibration volume
+		The volume position is defined relative to head psition (in a head-centered frame of reference)
+		Note that This just sets things up.  
+		Calibration points will not be visible until a calibration point is turned on
+		The volume is sampled linearly within the range
+		So, a resolution of 3 creates 27 points (3 samples along X * 3 along Y * 3 along Z)
+		'''
+		
+		x = np.linspace(xRange[0], xRange[1], resolution)
+		y = np.linspace(yRange[0], yRange[1], resolution)
+		z = np.linspace(zRange[0], zRange[1], resolution)
+
+		points = np.array([])
+		points.resize(1,3)
+		for i in x:
+			for j in y:
+				for k in z:
+					points = np.vstack((points, [i,j,k]))
+
+		points = np.delete(points, 0, 0)
+		self.calibrationPositions = points
+		self.numberOfCalibrationPoints = self.calibrationPositions.shape[0]
+		print 'calibration points', points
+		print '# of Calibration Points =', self.numberOfCalibrationPoints
+
+
+	def myCalibrationMethod(self):
+		
+		if ( self.calibrationInProgress == False):
+			self.calibrationInProgress = True
+			self.calibrationCounter = 0
+			self.calibrationSphere = vizshape.addSphere(self.calibrationSphereRadius, color = viz.PURPLE)
+			self.calibrationSphere.setParent(self.parentNode)
+			self.setSphereRadius(self.parentNode.getPosition(viz.ABS_GLOBAL), self.calibrationPositions[self.calibrationCounter,:], 0)
+			print 'FirstPos', self.calibrationPositions[self.calibrationCounter,:]
+			newPos = [self.calibrationPositions[self.calibrationCounter,0], self.calibrationPositions[self.calibrationCounter,1], self.calibrationPositions[self.calibrationCounter,2]]
+			self.calibrationSphere.setPosition(newPos[0], newPos[1], newPos[2],viz.ABS_PARENT)
+			print 'Calibration Procedure Started'
+		else:
+			self.calibrationInProgress = False
+			self.calibrationCounter = 0
+			self.calibrationSphere.remove()
+			print 'Quit Calibration!!'
+			
+	def updateCalibrationPoint(self):
+		
+		if(self.calibrationInProgress == True):
+			self.calibrationCounter +=1
+			if( self.calibrationCounter < self.numberOfCalibrationPoints ):
+				newPos = [self.calibrationPositions[self.calibrationCounter,0], self.calibrationPositions[self.calibrationCounter,1], self.calibrationPositions[self.calibrationCounter,2]]
+				self.calibrationSphere.setPosition(newPos[0], newPos[1], newPos[2],viz.ABS_PARENT)
+				self.setSphereRadius(self.parentNode.getPosition(viz.ABS_GLOBAL), self.calibrationPositions[self.calibrationCounter,:], 0)
+				#print 'Calibratring for Point[%d]' %(self.calibrationCounter), 'at [%f %f %f]' % (newPos[0], newPos[1], newPos[2])
+			else:
+				self.calibrationInProgress = False
+				self.calibrationCounter = 0
+				self.calibrationSphere.remove()
+				print 'Calibration Done Successfully'
+			
+		
+	def setSphereRadius(self, eyePos, targetPos, radius):
+
+		if ( radius  == 0 ):
+			distance = np.linalg.norm(targetPos)
+			self.calibrationSphereRadius = distance * np.tan(self.minimumAngle)
+			print 'Radius Changed to ', self.calibrationSphereRadius,' theta = ', 0.5 * 180 * np.arctan( self.calibrationSphereRadius/distance )/np.pi
+		else:
+			self.calibrationSphereRadius = radius
+			print '==>Radius Changed to ', self.calibrationSphereRadius
+		ratio = self.calibrationSphereRadius/0.02
+		self.calibrationSphere.setScale([ratio, ratio, ratio], viz.ABS_PARENT)
---
+++ main.py
@@ -0,0 +1,186 @@
+﻿from gazeTools import gazeSphere
+from gazeTools import gazeVector
+
+import viz
+import vizact
+import smi_beta
+import vizconnect
+
+
+
+# A demo of the gazeTools module
+# Some tools for visualizing data from the SMI eye tracker integration into the oculus rift dk2
+
+# Code by Kamran Binaee (with a bit of help from Gabriel Diaz) 
+# PerForm Labs @ Rochester Institute of Technology
+# Part of the multidisciplinary vision research laboratories (MVRL) 
+# ...in the Chester F. Carlson Center for Imaging Science!
+
+################## THE SETUP ########################
+
+# This code uses vizconnect file: vizConnectSetupFile.py
+#	This file is setup to configure both the oculus and a desktop mirror of what is inside the oculus
+#	The desktop mirror is a second instance of vizard run using the clustering tools that come with vizard enterprise
+
+# This configuratoin assumes that you have 2 monitors on the machine you're using, in addition to the Rift DK2
+# My monitor numbers correspond to...
+# 	1: oculus rift
+# 	2: for the vizard IDE
+# 	3: for a mirror of the in-helmet display
+# Windows not on the right monitors? You can move vizard windows between displays inside the vizconnect file. Just run that file with the vizard ide.
+
+# This configuatnoi also requires that you are using an enterprise version running the CLUSTER MASTER with 1 client running on the local machine
+#	Click on "tools".  
+#	In tools, click on "cluster master"
+#	Check the box next to localhost, and make sure that the count is 1
+#	You should probably uncheck "sync frames," if your desktop monitors can't run at 74 Hz, like your DK2 can
+
+
+
+
+def onKeyDown(key):
+	##########################################################
+	##########################################################
+	## Keys used in the default mode
+	
+	#  C brings up the SMI calibration screen
+	
+	
+	global eyeTracker, calibrationDoneSMI, text_object
+	if key == 'c' and eyeTracker:
+		
+		calibrationDoneSMI = 1.0
+		print 'calibrationDoneSMI ==> ', calibrationDoneSMI
+		eyeTracker.calibrate()
+
+	if key == 'e':
+		print 'My Calibration Method is Called'
+		cyclopeanCalibObj.myCalibrationMethod()
+		if ( cyclopeanCalibObj.calibrationInProgress == True ):
+			text_object = viz.addText('')
+			text_object.setParent(cyclopeanCalibObj.calibrationSphere)
+			vizact.onupdate(viz.PRIORITY_INPUT+1,cyclopeanCalibObj.calculateAngularError, cyclopEyeSphere.node3D, 0.0, text_object)#self.currentTrial.ballObj.node3D
+
+		else:
+			text_object.remove()
+
+	if key == 'q':
+
+		cyclopeanCalibObj.updateCalibrationPoint()
+
+
+# This specialized function allows you to update the mainview position using
+# the position informatnoi from a mocap system
+# and the orientation info from the oculus rift DK2
+def updateOrientation():
+	global headTracker
+	riftOriTracker = vizconnect.getTracker('rift').getNode3d()			
+	
+	headTracker = vizconnect.getRawTracker('headtracker')
+	
+	ori_xyz = riftOriTracker.getEuler()
+	headTracker.setEuler( ori_xyz  )
+	
+	# If you wanted to, you could now update the headTracker position using mocap data here
+	# We hardcode it a a particular value
+	headTracker.setPosition([0, 2, 0])
+
+
+# Register keypresses
+viz.callback(viz.KEYDOWN_EVENT, onKeyDown)
+
+# Import vizconnect file
+vizCon = vizconnect.go( 'vizConnect/' + 'vizConnectSetupFile.py' )
+
+# Import SMI eyetracker module
+simulateBool = True
+if(simulateBool):
+	print '***** SIMULATING GAZE DATA *******'
+eyeTracker = smi_beta.iViewHMD(simulate=simulateBool)
+
+
+# Import piazza environment
+viz.add('piazza.osgb')
+
+# Schedule call to update head position/orientation on each frame
+vizact.onupdate(viz.PRIORITY_LAST_UPDATE,updateOrientation)
+
+################################################################################################
+################################################################################################
+## Some examples of how to use the gazetools module
+
+# Get access to the client window
+dispDict = vizconnect.getRawDisplayDict()
+desktopDisplay = dispDict['custom_window']
+
+# Get acces to the head tracker, which is used to update the mainview
+headTracker = vizconnect.getRawTracker('headtracker')
+
+# Create a cyclopean gaze vector.  Rendered only to desktopDisplay
+# In other words, this is not visible inside the helmet
+# The "gaze sphere" is placed along the subject's gaze vector
+# The viz.BOTH_EYE argument makes this a cyclopean eye vecotr
+# For left eye, use viz.LEFT_EYE, and for right, viz.RIGHT_EYE
+cyclopEyeSphere = gazeSphere(eyeTracker,viz.BOTH_EYE,headTracker,[desktopDisplay],viz.GREEN)
+# Note that the sphere and vector are indepent
+cyclopEyeSphere.toggleUpdate()
+
+##################################################################################
+##################################################################################
+### Here, we create a calibration volume for the CYCLOPEAN eye
+# The volume position is defined relative to head psition (in a head-centered frame of reference)
+# Note that This just sets things up.  
+# Calibration points will not be visible until a calibration point is turned on (below)
+
+import vizshape
+
+# For convenience, place a node at the locatoin of the cyclopean eye
+cyclopEyeNode = vizshape.addSphere(radius=0.015, color = viz.GREEN)
+cyclopEyeNode.setParent(headTracker)
+cyclopEyeNode.visible(viz.OFF) # Make it invisible
+
+# Create a calibration toolset for the cyclopean eye
+from gazeTools import calibrationTools
+cyclopeanCalibObj = calibrationTools(cyclopEyeNode)
+
+# Now, setup the range/size of the calib volume
+xMinMax = [-0.5, 0.5];  # in meters
+yMinMax = [-0.2, 0.2];  # in meters
+zMinMax = [1.0,5.0]; 
+resolution = 3; # The volume is sampled linearly within the range
+# So, a resolution of 3 creates 27 points (3 samples along X * 3 along Y * 3 along Z)
+# These points are linearly distributed
+cyclopeanCalibObj.setVolume(xMinMax, yMinMax, zMinMax, resolution)
+
+################################################
+################################################
+
+# Get the helmet's IPD.  This is set using the 
+# oculus config application
+IOD = vizconnect.getRawTracker('rift').getIPD()
+
+# For convenience, create a node3D leftEyeNode at the locatoin of the left eye
+leftEyeNode = vizshape.addSphere(radius = 0.005, color = viz.BLUE)
+#leftEyeNode.visible(viz.OFF)
+leftEyeNode.setParent(headTracker)
+# shift it to the left by half the IOD. This shift is in head-centered coordinates
+leftEyeNode.setPosition(-IOD/2, 0, 0.0,viz.ABS_PARENT) 
+
+# Create a gaze sphere at a location along the gaze vector
+left_sphere = gazeSphere(eyeTracker,viz.LEFT_EYE,leftEyeNode,[desktopDisplay],sphereColor=viz.YELLOW)
+# Create a gaze VECTOR, extending from the eye to the gaze sphere
+leftGazeVector = gazeVector(eyeTracker,viz.LEFT_EYE,leftEyeNode,[desktopDisplay],gazeVectorColor=viz.YELLOW)
+left_sphere.toggleUpdate() 
+leftGazeVector.toggleUpdate()
+# Note:  A small issue.  The way things are set up, if you make the eye node invisible with #leftEyeNode.visible(viz.OFF)
+# you will also hide the leftGazeVector, which is a child of the leftEyeNode.  
+
+# create a node3D rightEyeNode
+rightEyeNode = vizshape.addSphere(radius = 0.005, color = viz.RED)
+#rightEyeNode.visible(viz.OFF) 
+rightEyeNode.setParent(headTracker)
+rightEyeNode.setPosition(IOD/2, 0, 0.0,viz.ABS_PARENT)
+right_sphere = gazeSphere(eyeTracker,viz.RIGHT_EYE,rightEyeNode,[desktopDisplay],sphereColor=viz.ORANGE)
+rightGazeVector = gazeVector(eyeTracker,viz.RIGHT_EYE,rightEyeNode,[desktopDisplay],gazeVectorColor=viz.ORANGE)
+right_sphere.toggleUpdate()
+rightGazeVector.toggleUpdate()
